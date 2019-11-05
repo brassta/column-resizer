a53a90124ffa293ec6656780af6c30ca87b51241
brassta <brassta@gmail.com>
2019-11-05 23:07:50 +0100

    verovatno ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,36 +24,46 @@ export class AppComponent implements OnInit {
   title = 'column-resizer1';
 
   numberOfColumns = 4;
+  tableWidth = 0;
   widths: number[];
   widthsPx: number[];
   minWidth = 30;
   lefts: number[];
+  leftsPx: number[];
+  offsetsPx: number[];
   resetPosition: { x: 0, y: 0 };
   colors = ['tomato', 'deepskyblue', 'beige', 'yellowgreen', 'burlywood', 'lightseagreen'];
   randomColors: string[];
 
   ngOnInit() {
-    const startWidth = 100 / this.numberOfColumns;
+    this.tableWidth = (document.querySelector('.columns') as HTMLElement).offsetWidth;
+    const startWidth = 200 / this.numberOfColumns;
     this.widths = this.setArrayOfElements(this.numberOfColumns, startWidth);
-    this.widthsPx = this.setArrayOfElements(this.numberOfColumns, 125);
+    this.widthsPx = this.setArrayOfElements(this.numberOfColumns, this.tableWidth / this.numberOfColumns);
+    this.offsetsPx = this.setArrayOfElements(this.numberOfColumns - 1, 0);
+    console.log(this.widths, this.widthsPx);
     this.lefts = this.setArray(this.numberOfColumns, 100 / this.numberOfColumns);
+    this.leftsPx = this.setArray(this.numberOfColumns, this.tableWidth / this.numberOfColumns);
+    console.log(this.lefts);
+    console.log(this.leftsPx);
+    console.log(this.offsetsPx);
     this.randomColors = this.randomColorArray(this.numberOfColumns);
+
   }
 
   dragHandleEnded($event: CdkDragEnd, indexOfDraggedHandler) {
     const handler = $event.source.element.nativeElement as HTMLElement;
     const parentElement = document.querySelector('[data-column-index="' + indexOfDraggedHandler + '"]');
     const parentWidth = (parentElement as HTMLElement).offsetWidth;
-    console.log('parent', parentWidth, parentElement);
+    // console.log('parent', parentWidth, parentElement);
     const offsetPx = +this.getTranslate3d(handler)[0].replace('px', '');
-    console.log('offsetPx', offsetPx);
-    console.log('remaining px', parentWidth / 2 + offsetPx);
+    // console.log('offsetPx', offsetPx);
+    // console.log('remaining px', parentWidth / 2 + offsetPx);
 
     const changedByPercent = offsetPx / parentWidth * 100;
-    console.log('percentage', changedByPercent);
-    // this.recalculateWidths(changedByPercent, indexOfDraggedHandler);
-    this.recalculateWidths2(parentWidth / 2 + offsetPx, indexOfDraggedHandler);
-    this.recalculateLefts(indexOfDraggedHandler);
+    // console.log('percentage', changedByPercent);
+    this.recalculateWidths(offsetPx, indexOfDraggedHandler);
+    // this.recalculateLefts(indexOfDraggedHandler);
     handler.style.transform = 'translate3d(0,0,0)';
   }
 
@@ -69,59 +79,38 @@ export class AppComponent implements OnInit {
     return this.setArray(count - 1, progression);
   }
 
-  private recalculateWidths2(actualWidthOfColumnAfterDrag: number, indexOfDraggedHandler: any) {
-    console.log(this.widthsPx);
-    const newWidthsPx = this.widthsPx.map((item, i) => {
-      console.log(item);
-      if (i === indexOfDraggedHandler) {
-        return actualWidthOfColumnAfterDrag;
-      } else if (i === indexOfDraggedHandler + 1) {
-        return (item * 2 - actualWidthOfColumnAfterDrag);
-      } else {
-        return item;
-      }
-    });
+  private recalculateWidths(offsetX: number, indexOfDraggedHandler: any) {
+    // console.log('widths', JSON.stringify(this.widthsPx));
+    console.log('offset', offsetX);
+    console.log('index', indexOfDraggedHandler);
+    const localOffset = this.offsetsPx[indexOfDraggedHandler] - offsetX;
+    this.offsetsPx[indexOfDraggedHandler] = offsetX;
+    console.log('localOfset', -localOffset);
+    console.log('@@@@@@@@@@ offsets', this.offsetsPx);
+    this.widthsPx[indexOfDraggedHandler] = this.widthsPx[indexOfDraggedHandler] - localOffset;
+    this.widthsPx[indexOfDraggedHandler + 1] = this.widthsPx[indexOfDraggedHandler + 1] + localOffset;
+    console.log('posle widths', this.widthsPx);
+    console.log('---------------------------------------');
+    const testWidths = this.widthsPx.map((item) => {
+      return item / this.tableWidth * 100 * 2
+    })
+    console.log('test widths', testWidths);
+    const testLefts = this.recalculateLefts(testWidths);
+    console.log(testLefts);
+    this.redraw(testWidths, testLefts)
 
-    this.widthsPx = [...newWidthsPx];
-
-    this.widths = newWidthsPx.map(item => {
-      return item / 500 * 100 * 2;
-    });
-    console.log('finale', this.widths);
-    this.resetPosition = {x: 0, y: 0};
   }
 
-  private recalculateWidths(percentualChangeOfWidth: number, indexOfSourceColumn) {
-    return;
-    const minWidth = this.minWidth / 50 * 100;
-    // console.log('minimalno', minWidth);
-    const remainingWidth = 200 / this.numberOfColumns;
-    let newnewWidths = this.widths.map((item, i) => {
-      if (i === indexOfSourceColumn) {
-        return item + percentualChangeOfWidth;
-      } else if (i === indexOfSourceColumn + 1) {
-        return item - percentualChangeOfWidth;
-      } else {
-        return item;
-      }
-    });
-    this.widths = [...newnewWidths];
-    newnewWidths = [];
-    // console.log('posle', this.widths);
-    // console.log('total', this.widths.reduce((sum, item) => {
-    //   return sum += item;
-    // }, 0));
 
-    this.resetPosition = {x: 0, y: 0};
-  }
-
-  private recalculateLefts(indexOfSourceColumn) {
+  private recalculateLefts(widths: number[]) {
     let sum = 0;
+    let internalLefts = this.setArray(this.numberOfColumns, 0);
+    console.log(internalLefts);
     for (let i = 0; i < this.numberOfColumns; ++i) {
-      this.lefts[i] = sum;
-      sum += this.widths[i] / 2;
+      internalLefts[i] = sum;
+      sum += widths[i] / 2;
     }
-    console.log('lefts', this.lefts);
+    return internalLefts;
   }
 
   private getTranslate3d(el) {
@@ -151,4 +140,10 @@ export class AppComponent implements OnInit {
   }
 
 
+  private redraw(newWidths: number[], newLefts: number[]) {
+    this.widths = [...newWidths];
+    this.lefts = [...newLefts];
+    this.offsetsPx = this.setArrayOfElements(this.numberOfColumns - 1, 0);
+    this.resetPosition = {x: 0, y: 0}
+  }
 }
