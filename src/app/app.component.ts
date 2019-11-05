4fd7ec7c112ce2b9d31232f5870b339b525a958e
Branko Stevanovic <brassta@gmail.com>
2019-11-05 12:14:55 +0100

    work at work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import {CdkDragEnd} from "@angular/cdk/drag-drop";
+import {CdkDragEnd} from '@angular/cdk/drag-drop';
+
+/**
+ na pocetku imas 4 kolone siroke po 25%
+ na pocetku imas 4 kolone siroke po 125px;
+ ------------------------------------------------
+ offset px je tacan uvek
+ u principu zanimaju te samo dve kolone, pre i posle handlera
+ ako nadjes koliko su siroke te dve kolone zajedno to je nekih 100% izmena
+ prva kolona je prva kolona + offset
+ druga kolona je druga kolona - offset
+ zbir prve i druge kolone trebalo bi da ostane isti
+ ostale te ne zanimaju
+ osvezi nizove sa sirinama kolona
+ */
 
 @Component({
   selector: 'agenzzia-root',
@@ -11,15 +25,17 @@ export class AppComponent implements OnInit {
 
   numberOfColumns = 4;
   widths: number[];
+  widthsPx: number[];
+  minWidth = 30;
   lefts: number[];
   resetPosition: { x: 0, y: 0 };
   colors = ['tomato', 'deepskyblue', 'beige', 'yellowgreen', 'burlywood', 'lightseagreen'];
   randomColors: string[];
 
   ngOnInit() {
-    const startWidth = 200 / this.numberOfColumns;
+    const startWidth = 100 / this.numberOfColumns;
     this.widths = this.setArrayOfElements(this.numberOfColumns, startWidth);
-    this.lefts = [0, 50];
+    this.widthsPx = this.setArrayOfElements(this.numberOfColumns, 125);
     this.lefts = this.setArray(this.numberOfColumns, 100 / this.numberOfColumns);
     this.randomColors = this.randomColorArray(this.numberOfColumns);
   }
@@ -28,37 +44,75 @@ export class AppComponent implements OnInit {
     const handler = $event.source.element.nativeElement as HTMLElement;
     const parentElement = document.querySelector('[data-column-index="' + indexOfDraggedHandler + '"]');
     const parentWidth = (parentElement as HTMLElement).offsetWidth;
+    console.log('parent', parentWidth, parentElement);
     const offsetPx = +this.getTranslate3d(handler)[0].replace('px', '');
+    console.log('offsetPx', offsetPx);
+    console.log('remaining px', parentWidth / 2 + offsetPx);
+
     const changedByPercent = offsetPx / parentWidth * 100;
-    this.recalculateWidths(changedByPercent, changedByPercent, indexOfDraggedHandler);
-    this.recalculateLefts(indexOfDraggedHandler)
+    console.log('percentage', changedByPercent);
+    // this.recalculateWidths(changedByPercent, indexOfDraggedHandler);
+    this.recalculateWidths2(parentWidth / 2 + offsetPx, indexOfDraggedHandler);
+    this.recalculateLefts(indexOfDraggedHandler);
     handler.style.transform = 'translate3d(0,0,0)';
   }
 
   setArray(count: number, progression: number) {
-    return Array(count).fill(0).map((x, i) => progression * i)
+    return Array(count).fill(0).map((x, i) => progression * i);
   }
 
   setArrayOfElements(count: number, element: any) {
-    return Array(count).fill(0).map((x) => element)
+    return Array(count).fill(0).map((x) => element);
   }
 
   setShortenedArrayByLastElement(count: number, progression) {
     return this.setArray(count - 1, progression);
   }
 
-  private recalculateWidths(percentualChangeOfWidth: number, offsetPercents: number, indexOfSourceColumn) {
-    const remainingWidth = 200 / this.numberOfColumns;
-    const newWidths = [this.widths[indexOfSourceColumn] + this.widths[indexOfSourceColumn] * percentualChangeOfWidth / 100 * 2
-    ];
-    this.widths[indexOfSourceColumn] = newWidths[0];
-    this.widths[indexOfSourceColumn + 1] = 200 - newWidths[0] - (this.numberOfColumns - 2) * remainingWidth;
-    console.log(this.widths);
-    console.log(this.widths.reduce((sum,item) => {
-      return sum += item
-    },0));
+  private recalculateWidths2(actualWidthOfColumnAfterDrag: number, indexOfDraggedHandler: any) {
+    console.log(this.widthsPx);
+    const newWidthsPx = this.widthsPx.map((item, i) => {
+      console.log(item);
+      if (i === indexOfDraggedHandler) {
+        return actualWidthOfColumnAfterDrag;
+      } else if (i === indexOfDraggedHandler + 1) {
+        return (item * 2 - actualWidthOfColumnAfterDrag);
+      } else {
+        return item;
+      }
+    });
 
-    this.resetPosition = {x: 0, y: 0}
+    this.widthsPx = [...newWidthsPx];
+
+    this.widths = newWidthsPx.map(item => {
+      return item / 500 * 100 * 2;
+    });
+    console.log('finale', this.widths);
+    this.resetPosition = {x: 0, y: 0};
+  }
+
+  private recalculateWidths(percentualChangeOfWidth: number, indexOfSourceColumn) {
+    return;
+    const minWidth = this.minWidth / 50 * 100;
+    // console.log('minimalno', minWidth);
+    const remainingWidth = 200 / this.numberOfColumns;
+    let newnewWidths = this.widths.map((item, i) => {
+      if (i === indexOfSourceColumn) {
+        return item + percentualChangeOfWidth;
+      } else if (i === indexOfSourceColumn + 1) {
+        return item - percentualChangeOfWidth;
+      } else {
+        return item;
+      }
+    });
+    this.widths = [...newnewWidths];
+    newnewWidths = [];
+    // console.log('posle', this.widths);
+    // console.log('total', this.widths.reduce((sum, item) => {
+    //   return sum += item;
+    // }, 0));
+
+    this.resetPosition = {x: 0, y: 0};
   }
 
   private recalculateLefts(indexOfSourceColumn) {
@@ -67,8 +121,7 @@ export class AppComponent implements OnInit {
       this.lefts[i] = sum;
       sum += this.widths[i] / 2;
     }
-    // console.log(this.lefts);
-    // this.lefts[indexOfSourceColumn + 1] = this.widths[indexOfSourceColumn] / 2 + this.lefts[indexOfSourceColumn];
+    console.log('lefts', this.lefts);
   }
 
   private getTranslate3d(el) {
@@ -80,20 +133,22 @@ export class AppComponent implements OnInit {
   }
 
   randomColorArray(numOfElements: number) {
-    let randomColors = [...this.colors];
+    const randomColors = [...this.colors];
     return this.shuffleArray(this.colors.length).map((index) => {
       return randomColors[index];
-    })
+    });
   }
 
   private shuffleArray(arrayLength) {
-    let resultArray = this.setArray(arrayLength, 1);
+    const resultArray = this.setArray(arrayLength, 1);
     for (let i = resultArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * i)
-      const temp = resultArray[i]
-      resultArray[i] = resultArray[j]
-      resultArray[j] = temp
+      const j = Math.floor(Math.random() * i);
+      const temp = resultArray[i];
+      resultArray[i] = resultArray[j];
+      resultArray[j] = temp;
     }
     return resultArray;
   }
+
+
 }
